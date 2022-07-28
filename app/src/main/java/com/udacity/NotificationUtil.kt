ee3b2b86f8c3d7cfdcc77f9adaaef8de5184386c
Valery Basova <basovavalery@gmail.com>
2022-07-28 15:43:01 -0400

    add PendingIntent to Open DetailActivity

--- app/src/main/java/com/udacity/NotificationUtil.kt
+++ app/src/main/java/com/udacity/NotificationUtil.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 
 private val NOTIFICATION_ID = 0
@@ -11,6 +13,15 @@ fun NotificationManager.sendNotification(
     applicationContext: Context,
     channelId: String
 ) {
+
+    val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
     val builder = NotificationCompat.Builder(
         applicationContext,
         channelId
@@ -21,6 +32,8 @@ fun NotificationManager.sendNotification(
                 .getString(R.string.notification_title)
         )
         .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
     notify(NOTIFICATION_ID, builder.build())
 }
